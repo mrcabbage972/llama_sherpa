a64a33e2c4da88ccad199673a2f4f95ea76bd60c
mrcabbage972 <mayvic@gmail.com>
2023-08-25 14:18:49 -0400

    Removing commented out code

--- main.py
+++ main.py
@@ -120,29 +120,6 @@ def get_status(task_id):
 def list_tasks():
     return task_list_tasks.run()
 
-# @app.post("/token", response_model=Token)
-# async def login_for_access_token(
-#     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-# ):
-#     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-#     if not user:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail="Incorrect username or password",
-#             headers={"WWW-Authenticate": "Bearer"},
-#         )
-#     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-#     access_token = create_access_token(
-#         data={"sub": user.username}, expires_delta=access_token_expires
-#     )
-#     return {"access_token": access_token, "token_type": "bearer"}
-
-
-# @app.get("/users/me/", response_model=User)
-# async def read_users_me(
-#     current_user: Annotated[User, Depends(get_current_active_user)]
-# ):
-#     return current_user
 
 @app.post('/login')
 def login(data: OAuth2PasswordRequestForm = Depends()):
