4cf2b763f077c074664c7a649b2a778783840e90
mrcabbage972 <mayvic@gmail.com>
2023-08-30 15:54:50 -0400

    Adding hash password utils (still not in use)

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,4 @@
+import bcrypt
 from fastapi_login import LoginManager
 
 from app.db.db import User, SessionLocal
@@ -22,3 +23,12 @@ def query_user(user_id: str):
     user = sess.query(User).where(User.username == user_id).one()
 
     return user.__dict__ if user else None
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+
+def hash_password(password: str) -> str:
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_password.decode('utf-8')
