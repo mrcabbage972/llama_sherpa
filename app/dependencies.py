ad3edbfb382160acff316260d76ca52dec748835
mrcabbage972 <mayvic@gmail.com>
2023-09-02 17:18:15 -0400

    User list only visible to superusers

---
+++ app/dependencies.py
@@ -0,0 +1,12 @@
+from fastapi import Depends, HTTPException
+
+from app.auth import manager
+
+
+def get_current_active_superuser(
+        current_user=Depends(manager)):
+    if not current_user['is_superuser']:
+        raise HTTPException(
+            status_code=400, detail="The user doesn't have enough privileges"
+        )
+    return current_user
